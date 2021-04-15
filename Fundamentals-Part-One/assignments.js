6700161b435bfbd523d8885689ee8b1a2c31df1f
madeleinedippenaar <madeleinedippenaar0@gmail.com>
2021-04-15 11:28:23 -0400

    values and variables

---
+++ Fundamentals-Part-One/assignments.js
@@ -0,0 +1,7 @@
+// Assignment 1 Values and Variables //
+let country = "United States of America";
+let continent = "North America";
+let population = 300000000;
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ Fundamentals-Part-One/script.js
@@ -0,0 +1,23 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Madeleine");
+console.log(23);
+
+let firstName = "Bob";
+console.log(firstName);
+
+// Variable name conventions //
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
