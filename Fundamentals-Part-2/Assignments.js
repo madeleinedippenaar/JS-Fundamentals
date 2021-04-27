028a2efec015fd233454bc454c25080fd3c45da3
madeleinedippenaar <madeleinedippenaar0@gmail.com>
2021-04-27 19:46:47 -0400

    assignment 2 complete

--- Fundamentals-Part-2/Assignments.js
+++ Fundamentals-Part-2/Assignments.js
@@ -1,5 +1,5 @@
 ///////////////////////////////////////
-// Coding Challenge 1 Functions
+// Coding Assignment 1 Functions
 
 function describeCountry(country, population, capitalCity) {
     return `${country} has ${population} million people and it's capital city is ${capitalCity}`;
@@ -12,3 +12,29 @@ const descSweden = describeCountry("Sweden", 10, "Stockholm")
 console.log(descUnitedStates);
 console.log(descEngland);
 console.log(descSweden);
+
+///////////////////////////////////////
+// Coding Assignment 2 Function declaration and expressions
+
+function percentageOfWorld1(population) {
+    return (population / 7900) * 100;
+}
+
+const populationUS = percentageOfWorld1(300);
+const populationEngland = percentageOfWorld1(55);
+const populationSweden = percentageOfWorld1(10);
+
+console.log(populationUS);
+console.log(populationEngland);
+console.log(populationSweden);
+
+const percentageOfWorld2 = function(population) {
+    return (population / 7900) * 100;
+}
+const populationUS1 = percentageOfWorld2(300);
+const populationEngland1 = percentageOfWorld2(55);
+const populationSweden1 = percentageOfWorld2(10);
+
+console.log(populationUS);
+console.log(populationEngland);
+console.log(populationSweden);
--- Fundamentals-Part-2/script.js
+++ Fundamentals-Part-2/script.js
@@ -35,3 +35,23 @@ console.log(fruitProcessor(5, 0));
 
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
+
+///////////////////////////////////////
+// Function Declarations and Expressions
+
+
+// Function declaration
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+}
+
+const age1= calcAge1(1993);
+
+// Function expression
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const age2 = calcAge2(1993);
+
+console.log(age1, age2);
